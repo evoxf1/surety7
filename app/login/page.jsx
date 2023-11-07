1f6a422afe750f391a44f760795cbd2493721256
evoxf1 <sahil2812@live.com>
2023-11-07 17:10:34 +0530

    adding auth

---
+++ app/login/page.jsx
@@ -0,0 +1,7 @@
+import LoginForm from "@/components/LoginForm";
+
+function page() {
+  return <LoginForm />;
+}
+
+export default page;
---
+++ app/register/page.jsx
@@ -0,0 +1,9 @@
+import RegisterForm from '@/components/RegisterForm'
+
+function page() {
+  return (
+    <RegisterForm/>
+  )
+}
+
+export default page
